6b62e7f92f243b628d923863286352000997687b
MrRuulici <raululici@yahoo.com>
2024-01-24 14:42:30 +0200

    Added readme

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,43 +1,43 @@
-import 'package:flutter/material.dart';
-import 'package:sole_market_project/pages/home_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:sole_market_project/pages/home_screen.dart';
 
 
-class Pages extends StatefulWidget {
-  @override
-  createState() => new PagesState();
-}
+// class Pages extends StatefulWidget {
+//   @override
+//   createState() => new PagesState();
+// }
 
-class PagesState extends State<Pages> {
-  int pageIndex = 0;
+// class PagesState extends State<Pages> {
+//   int pageIndex = 0;
 
-  // Create all the pages once and return same instance when required
-  final home_screen _profilePage = new ProfilePage(); 
-  final PlanPage _planPage = new PlanPage();
-  final StartUpNamerPage _startUpNamerPage = new StartUpNamerPage();
+//   // Create all the pages once and return same instance when required
+//   final home_screen _profilePage = new ProfilePage(); 
+//   final PlanPage _planPage = new PlanPage();
+//   final StartUpNamerPage _startUpNamerPage = new StartUpNamerPage();
 
 
-  Widget pageChooser() {
-    switch (this.pageIndex) {
-      case 0:
-        return _profilePage;
-        break;
+//   Widget pageChooser() {
+//     switch (this.pageIndex) {
+//       case 0:
+//         return _profilePage;
+//         break;
 
-      case 1:
-        return _planPage;
-        break;
+//       case 1:
+//         return _planPage;
+//         break;
 
-      case 2:
-        return _startUpNamerPage;
-        break;
+//       case 2:
+//         return _startUpNamerPage;
+//         break;
 
-      default:
-        return Container(
-          child: Center(
-              child: Text(
-                  'No page found by page chooser.',
-                  style: new TextStyle(fontSize: 30.0)
-              )
-          ),
-        );
-    }
-  }
+//       default:
+//         return Container(
+//           child: Center(
+//               child: Text(
+//                   'No page found by page chooser.',
+//                   style: new TextStyle(fontSize: 30.0)
+//               )
+//           ),
+//         );
+//     }
+//   }
